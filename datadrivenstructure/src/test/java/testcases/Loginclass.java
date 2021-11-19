01ac76b50af2934bc52a2baab9c9c7a2a802a562
Testvarun <74867222+varun-techie@users.noreply.github.com>
2021-11-19 09:56:04 +0530

    Update Loginclass.java

--- datadrivenstructure/src/test/java/testcases/Loginclass.java
+++ datadrivenstructure/src/test/java/testcases/Loginclass.java
@@ -22,6 +22,7 @@ public class Loginclass {
 	
 	
 	Genericwrapper gw= new Genericwrapper();
+	
 	Propreader pr= new Propreader();
 	Loginmodule lm = new Loginmodule();
 	
@@ -49,7 +50,7 @@ public class Loginclass {
 	//dataprovider
 	@Test(dataProviderClass = Excelreader.class,dataProvider = "myownfile",enabled=false)
 	public void secondcase(String myvalue) {
-		
+		System.out.println(myvalue);
 		System.out.println(myvalue);
 		
 	}
